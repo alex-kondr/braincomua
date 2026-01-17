2ae65bb627118211662cf1331d8f6d457e95b24a
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2026-01-17 18:37:30 +0200

    save

--- braincomua_project/braincomua_project/settings.py
+++ braincomua_project/braincomua_project/settings.py
@@ -80,11 +80,12 @@ WSGI_APPLICATION = 'braincomua_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": os.getenv("HOST"),
-        'OPTIONS': {'sslmode': 'require'},
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
